4528381e971a514774f81c94fba61c503a9ce7f2
byoukinn <1209418977@qq.com>
2018-10-31 18:27:32 +0800

    init repository

---
+++ src/RegisterUser.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URLDecoder;
+
+@WebServlet("/servlet/register")
+public class RegisterUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 此处拿到的是中文编码的内容，所以先设置请求的编码
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        System.out.println(URLDecoder.decode(username, "UTF-8"));
+
+        // 此处返回页面的编码是ISO-8859-1，我们把它设置成utf-8，解决中文乱码
+        resp.setContentType("text/html; charset=utf-8");
+        Writer wt = resp.getWriter();
+        wt.append(String.format("<h1>注册成功！%s，某某网站欢迎你</h1>", username));
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: BK
+  Date: 10/31/2018
+  Time: 5:08 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page pageEncoding="utf-8"  contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8"> <!-- for HTML5 -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>中文</title>
+</head>
+<body>
+<form action="servlet/register" method="get">
+    用户名: <input type="text" name="username">
+    <input type="submit">
+</form>
+
+</body>
+</html>
